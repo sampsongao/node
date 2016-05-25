2705ddb48247dc5bc09cd462cb92fb86a081de20
Sampson Gao <sampson.gao7@gmail.com>
2016-05-25 11:31:23 -0400

    Remove - V8 version < 4.5.8

--- include/nan/nan_callbacks_12_inl.h
+++ include/nan/nan_callbacks_12_inl.h
@@ -31,15 +31,7 @@ class ReturnValue {
 
   template <typename S> inline void Set(const Global<S> &handle) {
     TYPE_CHECK(T, S);
-#if defined(V8_MAJOR_VERSION) && (V8_MAJOR_VERSION > 4 ||                      \
-  (V8_MAJOR_VERSION == 4 && defined(V8_MINOR_VERSION) &&                       \
-  (V8_MINOR_VERSION > 5 || (V8_MINOR_VERSION == 5 &&                           \
-  defined(V8_BUILD_NUMBER) && V8_BUILD_NUMBER >= 8))))
     value_.Set(handle);
-#else
-    value_.Set(*reinterpret_cast<const v8::Persistent<S>*>(&handle));
-    const_cast<Global<S> &>(handle).Reset();
-#endif
   }
 
   // Fast primitive setters
